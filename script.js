12a1d5618e94b09755ea9de7cf0568b9e8feba45
jaytrive <113422452+jaytrive@users.noreply.github.com>
2023-03-16 19:25:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let output=document.getElementById('output')
+
+function updateText() {
+	return new Promise((resolve,reject) => {
+		setTimeout(() => {
+			resolve("Hello, world!")
+		},1000)
+	})
+}
+
+updateText().then((res) => output.innerText=res)
